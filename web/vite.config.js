977a1e8db7f64cd4dc02c4413766f298efc39db0
Ada Starnes <hxf168482@gmail.com>
2023-03-29 15:18:21 +0800

    add files

--- web/vite.config.js
+++ web/vite.config.js
@@ -11,6 +11,7 @@ import fs from 'fs'
 const envPrefix = 'WEAVE_';
 
 // https://vitejs.dev/config/
+<<<<<<< HEAD
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd(), envPrefix) };
 
@@ -20,6 +21,56 @@ export default ({ mode }) => {
         '@': resolve(__dirname, 'src'),
         'views': resolve(__dirname, 'src/views'),
         'components': resolve(__dirname, 'src/components'),
+=======
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      'views': resolve(__dirname, 'src/views'),
+      'components': resolve(__dirname, 'src/components'),
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/element.scss" as *;`,
+      },
+    },
+  },
+  plugins: [
+    vue(),
+    prismjs({
+      languages: ["json", "js", "go", "bash", "yaml", "markup"],
+      plugins: ["line-numbers"],
+      theme: "solarizedlight",
+      css: true,
+    }),
+    AutoImport({
+      resolvers: [ElementPlusResolver({ importStyle: "sass" })],
+    }),
+    Components({
+      resolvers: [
+        ElementPlusResolver({ importStyle: "sass" }),
+      ],
+    })
+  ],
+  server: {
+    // if your frontend not in the localhost, please uncomment the https config meanwhile
+    host: "0.0.0.0",
+    port: 8081,
+    https: {
+      ca: fs.readFileSync('../certs/root.crt'),
+      key: fs.readFileSync('../certs/frontend.key'),
+      cert: fs.readFileSync('../certs/frontend.crt')
+    },
+    proxy: {
+      '/api': {
+        target: 'https://api.tool.mybns.cn',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+        rewrite: (path) => path.replace(/\/api/, '/api')
+>>>>>>> 89eb60c (add files)
       }
     },
     css: {
